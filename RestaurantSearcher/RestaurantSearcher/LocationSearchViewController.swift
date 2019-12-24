4aa9eecd945b94acefd8430891f12d5110e35dcc
makotthi <gurifonnkatou@gmail.com>
2019-12-24 20:00:13 +0900

    現在地を取得できるようになった

---
+++ RestaurantSearcher/RestaurantSearcher/LocationSearchViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LocationSearchViewController.swift
+//  RestaurantSearcher
+//
+//  Created by 谷藤真 on 2019/12/24.
+//  Copyright © 2019 makoto. All rights reserved.
+//
+
+import UIKit
+
+class LocationSearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
