7a6a9a1fc3d58efa332a35859c889e6c901ed137
makotthi <gurifonnkatou@gmail.com>
2020-03-03 10:18:32 +0900

    RestaurantTableViewCellを作成

---
+++ RestaurantSearcher/RestaurantSearcher/ViewParts/RestaurantTableViewCell/RestaurantTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RestaurantTableViewCell.swift
+//  RestaurantSearcher
+//
+//  Created by 谷藤真 on 2020/03/03.
+//  Copyright © 2020 makoto. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
