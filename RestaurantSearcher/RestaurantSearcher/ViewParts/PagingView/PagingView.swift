252dcdba823b649a337b835b45704b77c28950e1
makotthi <gurifonnkatou@gmail.com>
2020-03-03 14:10:01 +0900

    UIパーツとコードを接続

--- RestaurantSearcher/RestaurantSearcher/ViewParts/PagingView/PagingView.swift
+++ RestaurantSearcher/RestaurantSearcher/ViewParts/PagingView/PagingView.swift
@@ -3,6 +3,11 @@ import UIKit
 
 class PagingView: UIView {
 
+    @IBOutlet private weak var backPageButton: UIButton!
+    @IBOutlet private weak var nextPageButton: UIButton!
+    @IBOutlet private weak var pageLabel: UILabel!
+    
+    
     /*
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
@@ -10,5 +15,10 @@ class PagingView: UIView {
         // Drawing code
     }
     */
-
+    @IBAction private func backPageButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction private func nextPageButtonTapped(_ sender: Any) {
+    }
+    
 }
