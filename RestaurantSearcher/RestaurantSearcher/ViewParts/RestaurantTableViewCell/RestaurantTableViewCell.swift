456bcb66807f300fe3ebb37f17893b4f36c9cd8e
makotthi <gurifonnkatou@gmail.com>
2020-03-03 10:39:47 +0900

    不要なメソッドを削除

--- RestaurantSearcher/RestaurantSearcher/ViewParts/RestaurantTableViewCell/RestaurantTableViewCell.swift
+++ RestaurantSearcher/RestaurantSearcher/ViewParts/RestaurantTableViewCell/RestaurantTableViewCell.swift
@@ -8,15 +8,6 @@ class RestaurantTableViewCell: UITableViewCell {
     @IBOutlet private weak var storeNameLabel: UILabel!
     @IBOutlet private weak var rootLabel: UILabel!
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
+    
     
 }
