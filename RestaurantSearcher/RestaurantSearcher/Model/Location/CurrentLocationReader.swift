98300202b581606c18314291245eaabf532a1f5e
makotthi <gurifonnkatou@gmail.com>
2020-03-04 12:04:48 +0900

    readCurrentLocationメソッドで、位置情報を取得する

--- RestaurantSearcher/RestaurantSearcher/Model/Location/CurrentLocationReader.swift
+++ RestaurantSearcher/RestaurantSearcher/Model/Location/CurrentLocationReader.swift
@@ -3,14 +3,19 @@
 import CoreLocation
 
 class CurrentLocationReader: NSObject{
+    
+    // CLLocationManagerのインスタンスを生成
+    private let locationManager = CLLocationManager()
+    
     // 位置情報を読み取って、緯度経度を返す
     func readCurrentLocation(_ handler: @escaping (Result<(Double, Double), Error>) -> Void) {
-        
+        // 位置情報を取得する
+        locationManager.requestLocation()
     }
 }
 
 extension CurrentLocationReader: CLLocationManagerDelegate{
-    // デリゲードメソッド
+    // Delegateメソッド
     
     // 位置情報取得に成功した時に呼ばれる
     func locationManager(_ manager: CLLocationManager, didUpdateHeading newHeading: CLHeading) {
