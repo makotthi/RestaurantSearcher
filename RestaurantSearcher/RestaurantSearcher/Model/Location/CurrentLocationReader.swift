ade8ad3ae10d98c982fb064a9d68a77b9accbcee
makotthi <gurifonnkatou@gmail.com>
2020-03-04 11:58:34 +0900

    CLLocationManagerDelegateのDelegateメソッドを作成

--- RestaurantSearcher/RestaurantSearcher/Model/Location/CurrentLocationReader.swift
+++ RestaurantSearcher/RestaurantSearcher/Model/Location/CurrentLocationReader.swift
@@ -2,9 +2,23 @@
 
 import CoreLocation
 
-class CurrentLocationReader{
+class CurrentLocationReader: NSObject{
     // 位置情報を読み取って、緯度経度を返す
     func readCurrentLocation(_ handler: @escaping (Result<(Double, Double), Error>) -> Void) {
+        
+    }
+}
+
+extension CurrentLocationReader: CLLocationManagerDelegate{
+    // デリゲードメソッド
+    
+    // 位置情報取得に成功した時に呼ばれる
+    func locationManager(_ manager: CLLocationManager, didUpdateHeading newHeading: CLHeading) {
+        
+    }
+    
+    // 位置情報取得に失敗した時に呼ばれる
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         
     }
 }
