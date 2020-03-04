f56e2d60c34ae7ddefdeff45d5f63fd93b65f170
makotthi <gurifonnkatou@gmail.com>
2020-03-04 12:08:46 +0900

    イニシャライザを設定

--- RestaurantSearcher/RestaurantSearcher/Model/Location/CurrentLocationReader.swift
+++ RestaurantSearcher/RestaurantSearcher/Model/Location/CurrentLocationReader.swift
@@ -7,6 +7,14 @@ class CurrentLocationReader: NSObject{
     // CLLocationManagerのインスタンスを生成
     private let locationManager = CLLocationManager()
     
+    // イニシャライザ
+    override init() {
+        super.init()
+        
+        // LocationManagerDelegateを設定
+        locationManager.delegate = self
+    }
+    
     // 位置情報を読み取って、緯度経度を返す
     func readCurrentLocation(_ handler: @escaping (Result<(Double, Double), Error>) -> Void) {
         // 位置情報を取得する
