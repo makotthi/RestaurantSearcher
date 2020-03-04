375e0a92752816c4415d49d819e51de159433fdc
makotthi <gurifonnkatou@gmail.com>
2020-03-04 16:03:12 +0900

    receiveRestaurantsメソッドの処理を追加 + コメントを追加

--- RestaurantSearcher/RestaurantSearcher/Model/APIClient/APIClient.swift
+++ RestaurantSearcher/RestaurantSearcher/Model/APIClient/APIClient.swift
@@ -1,7 +1,30 @@
 import UIKit
 
 class APIClient{
+    // レストランのデータをぐるなびAPIから受け取る
     func receiveRestaurants(_ url: URL, _ handler: @escaping (Result<StoreDataArray, Error>) -> Void){
-        
+        // リクエストに必要な情報を生成
+        let req = URLRequest(url: url)
+        // データ転送を管理するためのセッションを生成
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        // リクエストをタスクとして登録
+        let task = session.dataTask(with: req, completionHandler: {
+            (data, response, error) in
+            // セッションを終了
+            session.finishTasksAndInvalidate()
+            // do try catch エラーハンドリング
+            do {
+                // JSONDecoderのインスタンスを生成
+                let decoder = JSONDecoder()
+                // 受け取ったjsonデータをパースして格納
+                let json = try decoder.decode(StoreDataArray.self, from: data!)
+                
+                
+            } catch {
+                print("エラーが出ました")
+            }
+        })
+        // ダウンロード開始
+        task.resume()
     }
 }
