bc95daf3c7428f3d75f8363527256ff5a80c0ec9
makotthi <gurifonnkatou@gmail.com>
2020-03-04 12:25:40 +0900

    位置情報が取得できた時の処理を追加

--- RestaurantSearcher/RestaurantSearcher/Model/Location/CurrentLocationReader.swift
+++ RestaurantSearcher/RestaurantSearcher/Model/Location/CurrentLocationReader.swift
@@ -7,6 +7,8 @@ class CurrentLocationReader: NSObject{
     // CLLocationManagerのインスタンスを生成
     private let locationManager = CLLocationManager()
     
+    private var onCompleteReadCurrentLocation: ((Result<(Double, Double), Error>) -> Void)?
+    
     // イニシャライザ
     override init() {
         super.init()
@@ -27,7 +29,17 @@ extension CurrentLocationReader: CLLocationManagerDelegate{
     
     // 位置情報取得に成功した時に呼ばれる
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.first else {
+            return
+        }
         
+        // 緯度と経度を受け取る
+        let latitude = location.coordinate.latitude
+        let longitude = location.coordinate.longitude
+        
+        // 位置情報が取得できたことを伝える
+        // デリゲートからクロージャへ変換
+        onCompleteReadCurrentLocation?(.success((latitude, longitude)))
     }
     
     // 位置情報取得に失敗した時に呼ばれる
