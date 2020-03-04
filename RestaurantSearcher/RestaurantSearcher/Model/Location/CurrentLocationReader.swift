27dd2b0267480a9279bb55fb62d46beca443959f
makotthi <gurifonnkatou@gmail.com>
2020-03-04 11:55:13 +0900

    readCurrentLocationメソッドを作成

--- RestaurantSearcher/RestaurantSearcher/Model/Location/CurrentLocationReader.swift
+++ RestaurantSearcher/RestaurantSearcher/Model/Location/CurrentLocationReader.swift
@@ -3,5 +3,8 @@
 import CoreLocation
 
 class CurrentLocationReader{
-    
+    // 位置情報を読み取って、緯度経度を返す
+    func readCurrentLocation(_ handler: @escaping (Result<(Double, Double), Error>) -> Void) {
+        
+    }
 }
