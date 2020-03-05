e26fc92c86e6ff5c417cc714c3464a64acd98f52
makotthi <gurifonnkatou@gmail.com>
2020-03-05 11:17:20 +0900

    UIViewExtension.swiftにswift lintを適用

--- RestaurantSearcher/RestaurantSearcher/Extension/UIViewExtension.swift
+++ RestaurantSearcher/RestaurantSearcher/Extension/UIViewExtension.swift
@@ -1,17 +1,15 @@
-
-
 import UIKit
 
-extension UIView{
+extension UIView {
     // フェードインの処理
-    func fadeIn(duration: TimeInterval = 0.2, completed: (() -> ())? = nil) {
+    func fadeIn(duration: TimeInterval = 0.2, completed: (() -> Void)? = nil) {
         alpha = 0
         isHidden = false
-        
+
         UIView.animate(withDuration: duration,
             animations: {
                 self.alpha = 1
-            }) { finished in
+            }) { _ in
                 completed?()
         }
     }
