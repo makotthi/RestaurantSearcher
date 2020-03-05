3d6e2e929a697ab42f05a7c11023ba7397b18358
makotthi <gurifonnkatou@gmail.com>
2020-03-05 11:17:42 +0900

    APIClient.swiftにswift lintを適用

--- RestaurantSearcher/RestaurantSearcher/Model/APIClient/APIClient.swift
+++ RestaurantSearcher/RestaurantSearcher/Model/APIClient/APIClient.swift
@@ -1,18 +1,18 @@
 import UIKit
 
-class APIClient{
+class APIClient {
     // レストランのデータをぐるなびAPIから受け取る
-    func receiveRestaurants(_ url: URL, _ handler: @escaping (Result<StoreDataArray, Error>) -> Void){
+    func receiveRestaurants(_ url: URL, _ handler: @escaping (Result<StoreDataArray, Error>) -> Void) {
         // リクエストに必要な情報を生成
         let req = URLRequest(url: url)
         // データ転送を管理するためのセッションを生成
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
         // リクエストをタスクとして登録
         let task = session.dataTask(with: req, completionHandler: {
-            (data, response, error) in
+            (data, _, error) in
             // セッションを終了
             session.finishTasksAndInvalidate()
-            
+
             // API通信に失敗した時のエラーハンドリング
             if let error = error {
                 handler(.failure(error))
@@ -21,7 +21,7 @@ class APIClient{
             guard let data = data else {
                 return
             }
-            
+
             // do try catch エラーハンドリング
             do {
                 // JSONDecoderのインスタンスを生成
@@ -29,10 +29,10 @@ class APIClient{
                 // 受け取ったjsonデータをパースして格納
                 let json = try decoder.decode(StoreDataArray.self, from: data)
                 print("ヒット件数:\(String(describing: json.total_hit_count))")
-                
+
                 // クロージャを実行
                 handler(.success(json))
-                
+
             } catch {
                 print("エラーが出ました")
                 handler(.failure(error))
