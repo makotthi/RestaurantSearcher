f82ab681da0bd85a6fbf700a2534ceec4c7927b4
makotthi <gurifonnkatou@gmail.com>
2020-03-05 11:14:35 +0900

    DetailsViewController.swiftにswift lintを適用

--- RestaurantSearcher/RestaurantSearcher/DetailsViewController.swift
+++ RestaurantSearcher/RestaurantSearcher/DetailsViewController.swift
@@ -1,11 +1,9 @@
-
-
 import UIKit
 import SafariServices
 
-// MARK: -vars and initialize
+// MARK: - vars and initialize
 class DetailsViewController: UIViewController {
-    
+
     // 店の画像を表示するimageView
     @IBOutlet private weak var storeImageView1: UIImageView!
     @IBOutlet private weak var storeImageView2: UIImageView!
@@ -14,43 +12,39 @@ class DetailsViewController: UIViewController {
     @IBOutlet private weak var addressLabel: UILabel!
     @IBOutlet private weak var phoneNumberLabel: UILabel!
     @IBOutlet private weak var opentimeLabel: UILabel!
-    
-    
+
     // 選択したレストランのID
     var selectID: String?
-    
+
     // 店の詳細データ
     private var storeData: StoreData?
-    
+
     // APIクライアント
     private let apiClient = APIClient()
-    
-    
+
     // 画面遷移をした時に呼ばれる
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        
+
         // レストランのデータを受け取る
         receiveRestaurantData()
     }
-    
 
 }
 
-
-// MARK: -API
-extension DetailsViewController{
+// MARK: - API
+extension DetailsViewController {
     // API通信を行うメソッド
     // レストランのデータを受け取る
-    private func receiveRestaurantData(){
+    private func receiveRestaurantData() {
         if let id = selectID {
             // リクエストURLの組み立て
             guard let requestURL = URL(string: "https://api.gnavi.co.jp/RestSearchAPI/v3/?keyid=6cf2ac2af697b3358620582d34884f09&id=\(id)") else {
                 return
             }
-            
+
             // レストランのデータを受け取る
             apiClient.receiveRestaurants(requestURL, {[unowned self]  result in
                 switch result {
@@ -62,41 +56,40 @@ extension DetailsViewController{
                         // 画面上に店舗のデータを表示する
                         self.showStoreData()
                     }
-                    
+
                 // エラーが発生した時
                 case .failure(let error):
                     print(error)
                 }
             })
-            
+
         }
-        
+
     }
 }
 
-
-// MARK: -UI
-extension DetailsViewController{
+// MARK: - UI
+extension DetailsViewController {
     // 取得したデータを画面に表示する
-    private func showStoreData(){
+    private func showStoreData() {
         // 店舗画像を表示
         storeImageView1.loadImage(url: storeData?.image_url?.shop_image1)
         storeImageView2.loadImage(url: storeData?.image_url?.shop_image2, noImage: .ver2)
-        
+
         // 店舗名称を表示
         nameLabel.text = storeData?.name
         nameLabel.sizeToFit()
-        
+
         // 住所を表示
         addressLabel.text = storeData?.address
         addressLabel.sizeToFit()
-        
+
         // 電話番号を表示
         if let phoneNumber = storeData?.tel {
             phoneNumberLabel.text = "TEL: " + phoneNumber
         }
         phoneNumberLabel.sizeToFit()
-        
+
         // 営業時間
         if let openTime = storeData?.opentime {
             opentimeLabel.text = "営業時間:\n" + openTime
@@ -105,9 +98,8 @@ extension DetailsViewController{
     }
 }
 
-
-// MARK: -Action
-extension DetailsViewController{
+// MARK: - Action
+extension DetailsViewController {
     // もっと詳しくボタンが押された時の処理
     // レストランのWebページを表示する
     @IBAction private func showWebPageButtonAction(_ sender: Any) {
@@ -122,20 +114,19 @@ extension DetailsViewController{
     }
 }
 
-
-// MARK: -SFSafariViewControllerDelegate
-extension DetailsViewController: SFSafariViewControllerDelegate{
+// MARK: - SFSafariViewControllerDelegate
+extension DetailsViewController: SFSafariViewControllerDelegate {
     // SFSafariViewを開く
-    private func openSafariView(url: URL){
+    private func openSafariView(url: URL) {
         let safariViewController = SFSafariViewController(url: url)
-        
+
         // delegateの通知先を設定
         safariViewController.delegate = self
-        
+
         // safariViewが開かれる
         present(safariViewController, animated: true, completion: nil)
     }
-    
+
     // safariViewが閉じられた時に呼ばれるdelegateメソッド
     func safariViewControllerDidFinish(_ controller: SFSafariViewController) {
         // safariViewを閉じる
