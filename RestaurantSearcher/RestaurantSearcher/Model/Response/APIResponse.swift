417c2d0fd1e5e97c60e157c7af57e97d473c6b39
makotthi <gurifonnkatou@gmail.com>
2020-03-05 15:01:43 +0900

    StoreDataArrayのページの文字列を作成するメソッドを追加

--- RestaurantSearcher/RestaurantSearcher/Model/Response/APIResponse.swift
+++ RestaurantSearcher/RestaurantSearcher/Model/Response/APIResponse.swift
@@ -6,13 +6,27 @@ import  Foundation
 struct StoreDataArray: Codable {
     let total_hit_count: Int?
     let rest: [StoreData]?
-    
+
     // レスポンスのページ数を返す
     var totalPage: Int {
         guard let total = total_hit_count else {
             return 0
         }
         return Int(ceil(Double(total) / 100.0))
+    }
+    
+    // total_hit_countが値を持っているかどうかを判断
+    var hasResult: Bool {
+        totalPage > 0
+    }
+    
+    // ページの文字列を作る
+    func pageString(currentPage: Int) -> String {
+        if hasResult {
+            return "\(currentPage) /\(totalPage) ページ"
+        } else {
+            return "検索結果なし"
+        }
     }
 }
 // 店舗のデータを格納する
