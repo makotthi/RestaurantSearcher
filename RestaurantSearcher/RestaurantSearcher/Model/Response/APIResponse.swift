7473fd5c91d3429d4b9afe1f6ac5c32383e58f44
makotthi <gurifonnkatou@gmail.com>
2020-03-05 15:23:01 +0900

    hasNextPageメソッドとhasPreviousPageメソッドを追加

--- RestaurantSearcher/RestaurantSearcher/Model/Response/APIResponse.swift
+++ RestaurantSearcher/RestaurantSearcher/Model/Response/APIResponse.swift
@@ -14,12 +14,12 @@ struct StoreDataArray: Codable {
         }
         return Int(ceil(Double(total) / 100.0))
     }
-    
+
     // total_hit_countが値を持っているかどうかを判断
     var hasResult: Bool {
         totalPage > 0
     }
-    
+
     // ページの文字列を作る
     func pageString(currentPage: Int) -> String {
         if hasResult {
@@ -27,6 +27,16 @@ struct StoreDataArray: Codable {
         } else {
             return "検索結果なし"
         }
+    }
+
+    // 次のページがあるかどうかを返す
+    func hasNextPage(of page: Int) -> Bool {
+        page < totalPage
+    }
+
+    // 前のページがあるかどうかを返す
+    func hasPreviousPage(of page: Int) -> Bool {
+        page > 1
     }
 }
 // 店舗のデータを格納する
