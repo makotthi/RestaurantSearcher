bd6a566a0f348b53db6e7ab4df54d9e09fc6bb37
makotthi <gurifonnkatou@gmail.com>
2020-03-05 10:31:19 +0900

    fadeInメソッドの記述をUIViewExtension.swiftに移動

--- RestaurantSearcher/RestaurantSearcher/Extension/UIImageViewExtension.swift
+++ RestaurantSearcher/RestaurantSearcher/Extension/UIImageViewExtension.swift
@@ -37,18 +37,4 @@ extension UIImageView{
     }
 }
 
-// 画像のフェードインの処理
-extension UIView{
-    func fadeIn(duration: TimeInterval = 0.2, completed: (() -> ())? = nil) {
-        alpha = 0
-        isHidden = false
-        
-        UIView.animate(withDuration: duration,
-            animations: {
-                self.alpha = 1
-            }) { finished in
-                completed?()
-        }
-    }
-}
 
--- RestaurantSearcher/RestaurantSearcher/Extension/UIViewExtension.swift
+++ RestaurantSearcher/RestaurantSearcher/Extension/UIViewExtension.swift
@@ -3,5 +3,16 @@
 import UIKit
 
 extension UIView{
-    
+    // フェードインの処理
+    func fadeIn(duration: TimeInterval = 0.2, completed: (() -> ())? = nil) {
+        alpha = 0
+        isHidden = false
+        
+        UIView.animate(withDuration: duration,
+            animations: {
+                self.alpha = 1
+            }) { finished in
+                completed?()
+        }
+    }
 }
