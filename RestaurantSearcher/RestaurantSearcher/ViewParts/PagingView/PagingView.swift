a038eb0302d7233042d1f27c22229cebe4357648
makotthi <gurifonnkatou@gmail.com>
2020-03-05 11:18:41 +0900

    PagingView.swiftにswift lintを適用

--- RestaurantSearcher/RestaurantSearcher/ViewParts/PagingView/PagingView.swift
+++ RestaurantSearcher/RestaurantSearcher/ViewParts/PagingView/PagingView.swift
@@ -1,4 +1,3 @@
-
 import UIKit
 
 class PagingView: UIView {
@@ -6,52 +5,46 @@ class PagingView: UIView {
     @IBOutlet private weak var backPageButton: UIButton!
     @IBOutlet private weak var nextPageButton: UIButton!
     @IBOutlet private weak var pageLabel: UILabel!
-    
 
-    // MARK: -Static Method
+    // MARK: - Static Method
     static func fromXib() -> PagingView {
         // PagingViewのxibファイルをオブジェクト化
         let nib = UINib(nibName: "PagingView", bundle: nil)
         // nibをインスタンス化
         let pagingView = nib.instantiate(withOwner: nil, options: nil).first as! PagingView
-        
+
         return pagingView
     }
-    
-    
-    // MARK: -Closure
+
+    // MARK: - Closure
     // クロージャを定義
     var onTapBackPageButton: (() -> Void)?
     var onTapNextPageButton: (() -> Void)?
-    
-    
-    // MARK: -Action
+
+    // MARK: - Action
     @IBAction private func backPageButtonTapped(_ sender: Any) {
         onTapBackPageButton?()
     }
-    
+
     @IBAction private func nextPageButtonTapped(_ sender: Any) {
         onTapNextPageButton?()
     }
-    
-    
-    
-    // MARK: -Setter
-    
+
+    // MARK: - Setter
+
     // Buttonの有効状態のセッター
     // backPageButton
-    func setBackPageButtonEnabled(isEnabled: Bool){
+    func setBackPageButtonEnabled(isEnabled: Bool) {
         backPageButton.isEnabled = isEnabled
     }
     // nextPageButton
-    func setNextPageButtonEnabled(isEnabled: Bool){
+    func setNextPageButtonEnabled(isEnabled: Bool) {
         nextPageButton.isEnabled = isEnabled
     }
-    
+
     // pageLabelのテキストをセット
-    func setPageLabelText(text: String){
+    func setPageLabelText(text: String) {
         pageLabel.text = text
     }
-    
-    
+
 }
